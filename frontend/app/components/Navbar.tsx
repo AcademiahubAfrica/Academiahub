0e8fc12c3cef58563fa85194b975262de53496c3
Stephanie-17 <Stephanie>
2025-11-07 15:07:48 +0100

    feat: created Navbar component

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,139 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { FaBars } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const Navbar = () => {
+	const [openMenu, setOpenMenu] = useState(false);
+	const navlinks = [
+		{
+			id: 1,
+			name: "Home",
+			href: "/",
+		},
+		{
+			id: 2,
+			name: "Explore",
+			href: "/explore",
+		},
+		{
+			id: 3,
+			name: "Features",
+			href: "/features",
+		},
+		{
+			id: 4,
+			name: "How it works",
+			href: "/how-it-works", //change this after making the page
+		},
+		{
+			id: 5,
+			name: "Testimonials",
+			href: "/testimonials",
+		},
+		{
+			id: 6,
+			name: "About us",
+			href: "/aboutus", // or about
+		},
+		{
+			id: 7,
+			name: "FAQs",
+			href: "/faqs",
+		},
+	];
+	const pathname = usePathname();
+	return (
+		// NavBar notes: should there be a navbar on the authenticaation pages.. ie sign up and login
+		<nav className="flex relative items-center justify-between  px-[26px] bg-white py-5">
+			<div className="right  flex items-center gap-7">
+				{/* Menu Button */}
+				<FaBars
+					onClick={() => setOpenMenu(true)}
+					className="text-[18px]  min-[1140px]:hidden"
+				/>
+				{/* Logo */}
+				<Image
+					src={"/academiaHub-logo.png"}
+					alt="AcademiaHub logo"
+					width={157}
+					height={45}
+				/>
+			</div>
+
+			{/* Nav Links notes: font-size for nav links might be too small */}
+			<ul className="hidden min-[1140px]:flex gap-[30px] ">
+				{navlinks.map((link) => (
+					<li key={link.id}>
+						<Link
+							className={`text-[14px]  leading-[130%] ${
+								pathname === link.href &&
+								"bg-linear-to-r from-primary to-[#F8BD00] bg-clip-text text-transparent"
+							}`}
+							href={link.href}
+						>
+							{link.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+
+			{/* CTA Buttons  */}
+			<div className=" buttons hidden md:flex items-center gap-[18px]">
+				<Link href={"/signup"}>
+					<Button
+						variant="default"
+						size="lg"
+						className="font-normal cursor-pointer text-[14px] leading-[130%] w-[150px]"
+					>
+						Join for free
+					</Button>
+				</Link>
+				<Link href={"/login"}>
+					<Button
+						variant="secondary"
+						size="lg"
+						className="font-medium cursor-pointer border border-primary text-[16px] leading-[130%] w-[150px]"
+					>
+						Log in
+					</Button>
+				</Link>
+			</div>
+
+			{/* SIDE NAV */}
+
+			<div
+				className={`fixed inset-0 bg-black/50 z-40 min-[1140px]:hidden transition-opacity duration-300 ${
+					openMenu ? "opacity-100" : "opacity-0 pointer-events-none"
+				}`}
+				onClick={() => setOpenMenu(false)}
+			/>
+			<div
+				className={` z-50 side-nav top-0  bg-white fixed h-full left-0  min-[1140px]:hidden max-md:w-[220px] md:w-60 flex ${
+					openMenu ? "translate-x-0" : "-translate-x-full"
+				} transition-transform duration-700 `}
+			>
+				<ul className="flex flex-col gap-10 mt-11 ml-[25px] ">
+					{navlinks.map((link) => (
+						<li key={link.id}>
+							<Link
+								className={`text-[14px]  leading-[130%] ${
+									pathname === link.href &&
+									"bg-linear-to-r from-primary to-[#F8BD00] bg-clip-text text-transparent"
+								}`}
+								onClick={() => setOpenMenu(false)}
+								href={link.href}
+							>
+								{link.name}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 import { Toaster } from 'react-hot-toast';
 
 
@@ -27,8 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased p-4 `}
       >
+        <Navbar />
         {children}
         <Toaster position="top-right" reverseOrder={false} />
 
