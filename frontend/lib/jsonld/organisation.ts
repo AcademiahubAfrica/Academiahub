e2d7065fb6156e573c5640da512b182615649bb8
ochife <ochifeoge@gmail.com>
2026-01-24 17:40:48 +0100

    fix : remove console.log in organization.ts

--- frontend/lib/jsonld/organisation.ts
+++ frontend/lib/jsonld/organisation.ts
@@ -1,6 +1,5 @@
 export const siteUrl = process.env.NEXTAUTH_URL as string;
 
-console.log(siteUrl);
 export const organizationJsonLd = {
   "@context": "https://schema.org",
   "@type": "EducationalOrganization",
