878322ca4327bad2b89c3aba0def04c1f336c817
Mustaphamexy <mustaphamexy111@gmail.com>
2026-02-05 13:03:13 +0000

    refactor: Restructued Pages Into Client Component

--- frontend/app/faqs/page.tsx
+++ frontend/app/faqs/page.tsx
@@ -1,28 +1,17 @@
-import JoinUs from "@/components/JoinUs";
-import LandingFaq from "@/components/LandingFaq";
-import ContactSupport from "@/components/ContactSupport";
-import { faqsMetadata } from "../data/metadataExports";
+import { Suspense } from "react";
+import { faqData } from "@/app/data/faqData";
+import FAQContent from "@/components/faqs/faqContent";
 
-export const metadata = faqsMetadata;
+export const metadata = faqData;
+
 export default function FAQ() {
   return (
-    <main className="min-h-screen bg-linear-to-b from-gray-50 to-white">
-      {/* Search Section */}
-      <section className="bg-linear-to-br from-gray-300 via-yellow-200 to-gray-200 border-b border-gray-200">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 md:py-16">
-          <div className="text-center mb-8">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-900 mb-4">
-              Frequently Asked Questions
-            </h1>
-            <p className="text-lg md:text-xl text-gray-600 max-w-3xl mx-auto">
-              Find answers to common questions about AcademiaHub
-            </p>
-          </div>
-        </div>
-      </section>
-      <LandingFaq hideHeader={true} />
-      <ContactSupport />
-      <JoinUs />
-    </main>
+    <Suspense fallback={
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    }>
+      <FAQContent />
+    </Suspense>
   );
 }
