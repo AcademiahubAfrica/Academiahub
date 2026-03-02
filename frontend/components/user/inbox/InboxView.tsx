8216d1100ec6eab6628cdef71aa74d6ff46f413d
Jahakeme <jucalasitok@gmail.com>
2026-03-02 20:50:02 +0100

    fix(frontend): hide new conversation FAB during chat

--- frontend/components/user/inbox/InboxView.tsx
+++ frontend/components/user/inbox/InboxView.tsx
@@ -68,7 +68,9 @@ const InboxView = () => {
         )}
       </section>
 
-      <MobileNewFAB onConversationCreated={handleConversationCreated} />
+      {!conversationId && (
+        <MobileNewFAB onConversationCreated={handleConversationCreated} />
+      )}
     </>
   );
 };
