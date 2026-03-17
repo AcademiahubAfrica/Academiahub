40755806614708d6552fe41b59933a04440250f4
Jahakeme <jucalasitok@gmail.com>
2026-03-17 14:11:35 +0100

    fix(frontend): update profile page styling

--- frontend/components/user/profile/otherUserProfile/ProfileSectionOther.tsx
+++ frontend/components/user/profile/otherUserProfile/ProfileSectionOther.tsx
@@ -16,7 +16,7 @@ const ProfileSectionOther = ({ profile }: { profile: Profile }) => {
 
   return (
     <div className="md:bg-white md:m-4 md:py-6 md:px-4 rounded-2xl">
-      <div className="h-[77px] lg:h-[145px] relative bg-gradient-to-r from-primary/30 to-primary/10" />
+      <div className="h-19.25 lg:h-36.25 relative bg-linear-to-r from-primary/30 to-primary/10" />
       <div className="flex gap-4 flex-col md:flex-row">
         <Avatar className="border-[3px] hidden md:block border-white shadow-md h-10 w-10 lg:w-25 lg:h-25 -mt-5">
           <AvatarImage src={avatarSrc} alt={profile.name || "avatar"} />
@@ -30,11 +30,11 @@ const ProfileSectionOther = ({ profile }: { profile: Profile }) => {
                 <AvatarImage src={avatarSrc} alt={profile.name || "avatar"} />
                 <AvatarFallback>{initials}</AvatarFallback>
               </Avatar>
-              <h3>{profile.name}</h3>
+              <h3 className="text-xl font-normal leading-none">{profile.name}</h3>
             </div>
             <Button
               variant={"outline2"}
-              className="max-w-[95px] text-sm leading-3.5 font-normal"
+              className="max-w-23.75 text-sm leading-3.5 font-normal mr-2.5 border-[#1E3A8A] hover:bg-[#adadad] hover:text-current"
             >
               Message
             </Button>
