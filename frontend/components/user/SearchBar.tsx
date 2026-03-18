b25cf5ffc4fdd994c0f311e569f4d24319f0e618
Stephanie-17 <emenikesteph@gmail.com>
2026-03-18 17:06:22 +0100

    refactor(frontend): implemented live search

--- frontend/components/user/SearchBar.tsx
+++ frontend/components/user/SearchBar.tsx
@@ -4,43 +4,64 @@ import { useRouter, useSearchParams } from "next/navigation"
 import { Input } from "../ui/input"
 import { FaSearch } from "react-icons/fa"
 import { Button } from "../ui/button"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
 export default function SearchBar() {
   const [value, setValue] = useState('')
   const router = useRouter()
   const searchParams = useSearchParams()
 
-  
 
-  const handleSearch = (value : string) => {
+  const handleSearch = (searchValue: string) => {
     const params = new URLSearchParams(searchParams)
-    
-    if (value) {
-      params.set("search", value)
+
+    if (searchValue) {
+      params.set("search", searchValue)
     } else {
       params.delete("search")
     }
 
     router.push(`?${params.toString()}`)
-
-    setValue('')
   }
 
+  
+  useEffect(() => {
+    const delay = setTimeout(() => {
+      handleSearch(value)
+    }, 400) 
+
+    return () => clearTimeout(delay)
+  }, [value])
+
   return (
-    <div className="w-2/3 sm:flex  items-center gap-5 hidden  ">
-       <div className="sm:w-3/4  border h-10 flex items-center p-3 focus-within:border-2 focus-within:border-gray-600 rounded-3xl">
-      <FaSearch className="font-medium text-gray-400" />
-      <Input
-       onKeyDown={(e)=> e.key ===  'Enter' && handleSearch(value) }
-      type="text"
-      value={value}
-      placeholder="Search..."
-      onChange={(e)=>setValue(e.target.value)}
-      className="border-none  w-full rounded-3xl text-sm  focus-visible:ring-0 "
-    />
+    <div className="w-2/3 sm:flex items-center gap-5 hidden">
+      
+      <div className="sm:w-3/4 border h-10 flex items-center p-3 focus-within:border-2 focus-within:border-gray-600 rounded-3xl">
+        <FaSearch className="text-gray-400" />
+        
+        <Input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          
+    
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSearch(value)
+            }
+          }}
+
+          placeholder="Search..."
+          className="border-none w-full rounded-3xl text-sm focus-visible:ring-0"
+        />
+      </div>
+
+
+      <Button
+        onClick={() => handleSearch(value)}
+        className="h-10 w-[20%]"
+      >
+        Search
+      </Button>
     </div>
-     <Button variant="default" size="sm" className="h-10 w-[20%] stext-sm" onClick={()=>handleSearch(value)}>Search</Button>
-    </div>
-   
   )
 }
