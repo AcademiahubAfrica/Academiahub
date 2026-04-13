9d0c9576b2dd3d34a61c394992525d578993ead9
Stephanie-17 <emenikesteph@gmail.com>
2026-04-13 21:17:38 +0100

    fix(frontend): add truncate class to document title

--- frontend/components/landing/ExploreSection.tsx
+++ frontend/components/landing/ExploreSection.tsx
@@ -62,7 +62,7 @@ const ExploreSection = async ({
 									loading="eager"
 								/>
 								<div className="flex flex-col gap-2.5 w-74">
-									<h4 className="font-medium  max-[1290px]:text-[16px] min-[1290px]:text-[18px] leading-[130%]">
+									<h4 className="font-medium  max-[1290px]:text-[16px] min-[1290px]:text-[18px] leading-[130%] truncate">
 										{doc.title}
 									</h4>
 									<div className="flex items-center gap-1.5">
