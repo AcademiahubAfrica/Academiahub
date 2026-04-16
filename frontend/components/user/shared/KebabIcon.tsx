172b55d169c8ed0f0a662e22de72d2cfdecf82ce
ochife <ochifeoge@gmail.com>
2026-04-16 19:52:50 +0100

    style(frontend): Allign kebab icon at bottom

--- frontend/components/user/shared/KebabIcon.tsx
+++ frontend/components/user/shared/KebabIcon.tsx
@@ -14,28 +14,37 @@ interface KebabProps {
   documentId?: string;
 }
 
-const KebabIcon = memo(function KebabIcon({ isOwnDocument, documentId }: KebabProps) {
+const KebabIcon = memo(function KebabIcon({
+  isOwnDocument,
+  documentId,
+}: KebabProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <GoKebabHorizontal className="-rotate-90 absolute max-sm:top-4 max-sm:left-2 max-lg:top-8 max-lg:left-4 lg:top-8 lg:left-4 cursor-pointer  text-white  z-50" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="p-0 rounded-none  w-27.5">
-        <DropdownMenuItem className="border-[#AEAEAE]  border-b h-10 flex justify-center items-center rounded-none">
+      <DropdownMenuContent
+        side="bottom"
+        align="start"
+        sideOffset={5}
+        className="p-0 rounded-none w-22  md:w-27.5 "
+      >
+        <DropdownMenuItem className="border-[#AEAEAE]  border-b max-sm:text-xs h-7 md:h-10 flex justify-center items-center rounded-none">
           Share
         </DropdownMenuItem>
         {isOwnDocument ? (
           <DropdownMenuItem
             variant="destructive"
-            className="border-[#AEAEAE] border-b h-10 flex justify-center items-center rounded-none"
+            className="border-[#AEAEAE] border-b max-sm:text-xs h-7 md:h-10 flex justify-center items-center rounded-none"
           >
             Delete
           </DropdownMenuItem>
         ) : (
-          <DropdownMenuItem asChild className="border-[#AEAEAE] border-b h-10 flex justify-center items-center rounded-none">
-            <Link href={`/publication/${documentId}/report-issue`}>
-              Report
-            </Link>
+          <DropdownMenuItem
+            asChild
+            className="border-[#AEAEAE] border-b max-sm:text-xs h-7 md:h-10 flex justify-center items-center rounded-none"
+          >
+            <Link href={`/publication/${documentId}/report-issue`}>Report</Link>
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
